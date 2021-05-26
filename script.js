d2e36d9b4fabccf105f87094f14278e37b7a82ca
desecaw13 <47424966+desecaw13@users.noreply.github.com>
2021-05-25 22:37:37 -0700

    added evil.css

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// javascript:(function(d,l){l=d.createElement("link");l.rel="stylesheet";l.href="https://rawgithub.com/tlrobinson/evil.css/master/evil.css";d.body.appendChild(l)})(document);
+"use strict";
+
+var style = document.createElement("link");
+style.disabled = true;
+style.id = "evilcss";
+style.type = "text/css";
+style.rel = "stylesheet";
+style.href = "https://rawgithub.com/tlrobinson/evil.css/master/evil.css";
+document.head.appendChild(style);
+
+function evilFlip() {
+    let checkBox = document.getElementById("eCB");
+    if (checkBox.checked) { style.disabled = false; }
+    else { style.disabled = true; }
+}
