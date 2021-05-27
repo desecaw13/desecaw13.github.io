6f0f5c69ff3fc6289012217022d78a431eaa79e3
desecaw13 <47424966+desecaw13@users.noreply.github.com>
2021-05-27 11:24:03 -0700

    add code for background

--- script.js
+++ script.js
@@ -8,6 +8,15 @@ function evilFlip() {
     else { styleLink.disabled = true; };
 }
 
-/*setInterval(function () {
-    document.body.style.background = "linear - gradient(#ccc, #777);" // todo change
-}, 1000);*/
+/*/ background animation
+let gtop = 191
+let gbot = 64
+let i = 1
+setInterval(function () {
+    if (gtop >= 255) { i = -1 } else if (gtop <= 0) { i = 1 };
+    if (gbot <= 0) { i = -1 } else if (gbot >= 255) { i = 1 };
+    //document.getElementsByTagName("iframe")[for].style.background
+    document.body.style.background = `linear-gradient(rgb(${gtop},${gtop},${gtop}), rgb(${gbot},${gbot},${gbot}))`;
+    gtop += i
+    gbot -= i
+}, 100);*/
