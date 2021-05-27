a5854ff3bf940ca3f9f51eb4c001d7b7230a14e6
desecaw13 <47424966+desecaw13@users.noreply.github.com>
2021-05-26 18:48:46 -0700

    done thongs

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 "use strict";
 
-var style = document.createElement("link");
-style.disabled = true;
-style.id = "evilcss";
-style.type = "text/css";
-style.rel = "stylesheet";
-style.href = "https://rawgithub.com/tlrobinson/evil.css/master/evil.css";
-document.head.appendChild(style);
-
+// evil.css
+let styleLink = document.getElementById("eCSS");
 function evilFlip() {
     let checkBox = document.getElementById("eCB");
-    if (checkBox.checked) { style.disabled = false; }
-    else { style.disabled = true; }
+    if (checkBox.checked) { styleLink.disabled = false; }
+    else { styleLink.disabled = true; };
 }
